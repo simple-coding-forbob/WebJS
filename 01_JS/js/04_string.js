39c56349dbb0903decaea6253d49275270ce9600
simple-coding-forbob <96178104+simple-coding-forbob@users.noreply.github.com>
2025-02-03 17:47:16 +0900

    js 업로드 2
    js 업로드 2

--- 01_JS/js/04_string.js
+++ 01_JS/js/04_string.js
@@ -1,16 +1,16 @@
 // TODO: 자료의 형태와 자료를 넣어두는 곳
 // TODO: 1) 홍길동 을 화면에 대화상자로 표시하세요
-let a = "홍길동";
-alert(value);  // 홍길동
+// let a = "홍길동";
+// alert(a);  // 홍길동
 
 // TODO: 2) 덧셈 연산자로 글자를 붙여서 대화상자로 표시하세요
 // TODO:  덧셈 연산자: +
-let b = "홍길동" + "만세";
-alert(b);      // 홍길동만세
+// let b = "홍길동" + "만세";
+// alert(b);      // 홍길동만세
 
 // TODO: 3) 논리합(or, 또는)
-let c = true || false;
-alert(c);
+// let c = true || false;
+// alert(c);
 
 // TODO: 4) 논리곱(and, 그리고)
 let d = true && false;
