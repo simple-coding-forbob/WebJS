42fd221293d4e75b47e87d798f06fc19995d4a5e
OGSuJin <forbobw@naver.com>
2025-02-15 17:19:26 +0900

    1

--- 04_FINAL_QUIZ/js/02_final_quiz.js
+++
@@ -1,6 +0,0 @@
-// TODO: section 안에 h1 자식태그만 선택해서 
-// TODO: background-color 을 blue 로 color 는 white 로 바꾸세요
-$(function(){
-    $("section").children("h1").css("background-color", "skyblue")
-                                .css("color", "white");
-});
--- 04_FINAL_QUIZ/js/03_final_quiz.js
+++
@@ -1,6 +0,0 @@
-// TODO: #quiz 을 click 하면 title 의 color 를 blue 로 바꾸세요
-$(function(){
-    $("#quiz").click(function() {
-        $("#title").css("color","blue");
-    })
-});
